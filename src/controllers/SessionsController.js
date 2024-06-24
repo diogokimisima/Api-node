a7714352812d3b2259f157a1e7f5cdd95123b71f
dkimisima@gmail.com <dkimisima@gmail.com>
2024-06-24 10:17:55 -0300

    fixing errors

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,31 +1,32 @@
 const knex = require("../database/knex");
-const AppError  = require("../utils/AppError");
+const AppError = require("../utils/AppError");
 const { compare } = require("bcryptjs");
 const authConfig = require("../config/auth");
 const { sign } = require("jsonwebtoken");
 
 class SessionsController {
     async create(request, response) {
-        const { email, password } = request.body;
+        let { email, password } = request.body;
+        
+        email = email.toLowerCase();
+        const user = await knex("users").whereRaw("LOWER(email) = ?", [email]).first();
 
-        const user = await knex("users").where({ email }).first();
-
-        if(!user) {
-            throw new AppError("E-mail e/ou senha incorreta", 401)
+        if (!user) {
+            throw new AppError("E-mail e/ou senha incorreta", 401);
         }
 
         const passwordMatched = await compare(password, user.password);
 
-        if(!passwordMatched){
-            throw new AppError("E-mail e/ou senha incorreta", 401)
+        if (!passwordMatched) {
+            throw new AppError("E-mail e/ou senha incorreta", 401);
         }
 
-        const { secret, expiresIn} = authConfig.jwt;
+        const { secret, expiresIn } = authConfig.jwt;
         const token = sign({}, secret, {
             subject: String(user.id),
-            expiresIn
-        })
-        
+            expiresIn,
+        });
+
         return response.json({ user, token });
     }
 }
